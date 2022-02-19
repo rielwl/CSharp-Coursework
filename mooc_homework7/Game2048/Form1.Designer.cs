4c12247855c2e2c2f20702f9b6a3ec7a44411ccb
rielwl <sherie.low.wl@gmail.com>
2022-02-20 00:41:58 +0800

    MOOC homework seven

---
+++ mooc_homework7/Game2048/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Game2048
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBoard = new System.Windows.Forms.Panel();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.lblHighScore = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlBoard
+            // 
+            this.pnlBoard.Location = new System.Drawing.Point(18, 155);
+            this.pnlBoard.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pnlBoard.Name = "pnlBoard";
+            this.pnlBoard.Size = new System.Drawing.Size(414, 455);
+            this.pnlBoard.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(24, 92);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(31, 36);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 36);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Score";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(15, 25);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(66, 24);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "数字";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(15, 53);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(66, 24);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "朝代";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // lblHighScore
+            // 
+            this.lblHighScore.AutoSize = true;
+            this.lblHighScore.Font = new System.Drawing.Font("Times New Roman", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighScore.Location = new System.Drawing.Point(25, 23);
+            this.lblHighScore.Name = "lblHighScore";
+            this.lblHighScore.Size = new System.Drawing.Size(115, 25);
+            this.lblHighScore.TabIndex = 5;
+            this.lblHighScore.Text = "High Score:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(496, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(192, 148);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "游戏模式";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(609, 177);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(79, 32);
+            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(15, 83);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(66, 24);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "官职";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(15, 113);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(66, 24);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "军职";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUp.Location = new System.Drawing.Point(546, 400);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(78, 72);
+            this.btnUp.TabIndex = 8;
+            this.btnUp.Text = "↑";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLeft.Location = new System.Drawing.Point(455, 484);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(78, 72);
+            this.btnLeft.TabIndex = 9;
+            this.btnLeft.Text = "←";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDown.Location = new System.Drawing.Point(546, 484);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(78, 72);
+            this.btnDown.TabIndex = 10;
+            this.btnDown.Text = "↓";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRight.Location = new System.Drawing.Point(640, 484);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(78, 72);
+            this.btnRight.TabIndex = 11;
+            this.btnRight.Text = "→";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 635);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblHighScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pnlBoard);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBoard;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label lblHighScore;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnRight;
+    }
+}
+
---
+++ mooc_homework7/Game2048/README.txt
@@ -0,0 +1,69 @@
+﻿阅读报告：
+老师的代码使用了Button[]和数组来实现 （主要）
+
+Form1_Load
+在form显示之前的一些代码
+
+StartGame
+把界面设置好，要create两个号码
+
+InitialUI
+用于 GenerateAllButtons，也就是设置好全部的2048游戏4*4的板中的按钮
+这里把buttons都生成好，位置都设置好，各种属性也在这里设置好
+是直接用for循环，循环到全部的按钮，设置好
+注意这里使用的GetTextOfButton和GetColorOfButton
+
+RefreshUI
+用RefreshAllButtons
+
+RefreshAllButtons
+用于换一下显示的按钮
+与InitialUI不同的是这个不会再定义button，是直接改变button的颜色和text
+
+GetTextOfButton
+在一开始定义的gameMode就是在这里使用
+之前老师定义了str1, str2, str3， 都是存不同游戏模式的Text，这里会先检查是哪个游戏模式
+int n 是要查什么位置的button，然后返回的是n位置上的按钮的值对应的button text
+button text是按着什么游戏模式+button值来决定的
+
+GetColorOfButton
+这个和GetTextOfButton相似，只是不需要查GameMode，只需要查button对应的值，然后返回对应的颜色
+
+ProcessCmdKey
+用于处理键盘上按left, right, up, down的结果
+是在这里掌控游戏， 玩游戏
+bMoved是个boolean，查是否有动，如果有动的话才需要改界面和查游戏是否结束
+if(bMoved)... 用于处理做了一个可行的动作后需要刷新界面、查游戏是否结束等操作
+
+rightMove, leftMove, upMove, downMove
+都是用于实现四个方向的移动
+
+generateNewData
+用于随机产生数字， 先找空的位置，再在那里生成一个数字
+
+IsGameOver
+查游戏是否已经结束
+
+
+
+所改的代码：
+显示分数
+加了一个显示分数的部分， 使用了老师定义的score
+
+选择模式
+使用了 radiobutton和 groupbox与一个submit按钮来实现了选择模式
+模式有四种（数字、朝代、官职、军职)， 都是跟着老师定义的模式, 使用了gameMode
+具体代码可以看btnSubmit_Click
+
+记录高分功能
+在Highscore.txt中记录最高分
+逻辑就是一开始游戏(Form1_load)的时候会开Highscore.txt，查记录的highScore
+然后再Form1_FormClosing的时候查score>highScore，若是就StreamWriter开起Highscore.txt，写入score
+也在ProcessCmdKey中如果判断IsGameOver()为真，也查if(score>highScore)，
+写入Highscore.txt和显示一个MessageBox，祝贺用户
+
+其他方面的修改
+修改了颜色，在GetColorOfButton中和Form1_Load中定义
+写了加了用于玩游戏的按钮，在一个region里 (这个可能需要找一下开...，在ProcessCmdKey后)
+也不难，跟着老师写的ProcessCmdKey，定义一个aftBtnCheck来查(老师在代码里写了这部分的逻辑)
+然后加了四个按钮, btnUp_Click, btnDown_Click, btnLeft_Click, btnRight_Click
