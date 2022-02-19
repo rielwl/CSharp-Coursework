dcc308c348ea5a86edb090c553c403298d25e81a
rielwl <sherie.low.wl@gmail.com>
2022-02-20 01:04:29 +0800

    MOOC homework nine

---
+++ mooc_homework9/homework9/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace homework9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(36, 256);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(271, 31);
+            this.txtID.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 301);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "输入身份证号码来验证";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PapayaWhip;
+            this.ClientSize = new System.Drawing.Size(338, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ mooc_homework9/homework9/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace homework9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "验证";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = txtID.Text;
+            string idpattern = @"^\d{17}(?:d|X)$"; //first part is the 17 digits, second is the last digit in the ID
+            int[] weights = { 7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2 };
+            string[] lastDigit = { "1", "0", "X", "9", "8", "7", "6", "5", "4", "3", "2" };
+            int sum = 0; 
+            if(id.Length != 18)
+            {
+                MessageBox.Show("身份证长度不对！");
+                return;
+            }
+            
+
+            
+            if(Regex.IsMatch(id, idpattern))
+            {
+                for (int i = 0; i < 17; i++)
+                {
+                    sum += weights[i] * int.Parse(id[i].ToString());
+                }
+                int result = sum % 11;
+                if (lastDigit[result] == id[17].ToString())
+                {
+                    MessageBox.Show("身份证格式正确！");
+                }
+                else
+                {
+                    MessageBox.Show("最后一位有错误！");
+                }
+            }
+            else
+            {
+                MessageBox.Show("身份证格式错误!");
+            }
+            txtID.Text = "";
+        }
+    }
+}
